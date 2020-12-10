f21dff5cc9be4005a0950960871ae54f0c6a8b5e
Mike Conroy <mikecon@outlook.com>
2020-12-10 22:28:00 +0000

    Day 9 - Part 2 Complete.

--- day9/day9.py
+++ day9/day9.py
@@ -2,7 +2,7 @@ f = open("input", "r")
 f = f.read()
 inputs = [int(x) for x in f.split("\n")]
 
-preambleLength = 25
+PREAMBLE_LENGTH = 25
 
 def containsSum(queue, target):
     if max(queue) <= target / 2:
@@ -16,20 +16,40 @@ def containsSum(queue, target):
 
 def solvePart1():
     queue = []
-    for i in range(0, preambleLength):
+    for i in range(0, PREAMBLE_LENGTH):
         queue.append(inputs[i])
     
-    for index in range(preambleLength, len(inputs)):
+    for index in range(PREAMBLE_LENGTH, len(inputs)):
         if containsSum(queue, inputs[index]):
             queue.pop(0)
             queue.append(inputs[index])
         else:
             return str(inputs[index])
-
-    return "1"
+    return "FAIL"
 
 def solvePart2():
-    return "2"
+    target = int(solvePart1())
+    contiguousNumbers = []
+    for index in range(0, len(inputs)):
+        contiguousNumbers = [inputs[index]]
+        resultFound = False
+        counter = 0
+        while not resultFound:
+            sumOfCurrentNums = sum(contiguousNumbers)
+            if sumOfCurrentNums == target:
+                resultFound = True
+                break
+            elif sumOfCurrentNums > target:
+                resultFound = False
+                break
+            else:
+                resultFound = False
+                counter += 1
+                contiguousNumbers.append(inputs[index + counter])
+        if resultFound:
+            return str(max(contiguousNumbers) + min(contiguousNumbers))
+
+    return "FAIL"
 
 print("Part 1: " + solvePart1())
 print("Part 2: " + solvePart2())
