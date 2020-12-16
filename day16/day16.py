a0cb64aa28055da58a95762540fe5881161b954e
Mike Conroy <mike.conroy@appirio.com>
2020-12-16 15:19:12 +0000

    Day 16 - Part 1 Complete.

--- day16/day16.py
+++ day16/day16.py
@@ -7,6 +7,9 @@ class Field:
         print("FIELD: " + self.name + " VALID VALUES: " + str(self.validValues))
 
 fields = []
+#Capture all valid values in a set
+
+setOfTotalValidValues = set()
 f = open("input", "r")
 f = f.read()
 input = f.split("\n\n")
@@ -20,6 +23,7 @@ for field in ticketFields.split("\n"):
         min = int(vRange.split("-")[0])
         max = int(vRange.split("-")[1])
         values = list(range(min, max+1))
+        setOfTotalValidValues.update(values)
         validValues += values
     fields.append(Field(fieldName, validValues))
 
@@ -39,14 +43,34 @@ nearbyTicketsInput.pop(0)
 for ticket in nearbyTicketsInput:
     nearbyTickets.append(parseTicket(ticket))
 
+# Input is:
+# fields - Array of Field objects containing name and valid values.
+# myTicket - Array of field values for my ticket.
+# nearbyTickets - Array of tickets (tickets represented as an array of field values)
+
 def solvePart1():
-    # Input is:
-    # fields - Array of Field objects containing name and valid values.
-    # myTicket - Array of field values for my ticket.
-    # nearbyTickets - Array of tickets (tickets represented as an array of field values)
-    return "1"
+    # Check all nearby tickets for any values that aren't valid in any of fields.
+
+    invalidNumbers = 0
+    for ticket in nearbyTickets:
+        for value in ticket:
+            if not value in setOfTotalValidValues:
+                invalidNumbers += value
+
+    return str(invalidNumbers)
 
 def solvePart2():
+    # Discard tickets that aren't valid
+    validNearbyTickets = []
+    for ticket in nearbyTickets:
+        validTicket = True
+        for value in ticket:
+            if value not in setOfTotalValidValues:
+                validTicket = False
+                break
+        if validTicket:
+            validNearbyTickets.append(ticket)
+
     return "2"
 
 print("Part 1: " + solvePart1())
