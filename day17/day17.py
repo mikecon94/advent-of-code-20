a5351e82034b4aa80012d6ed4dfd01c4b0249ec9
Mike Conroy <mikecon@outlook.com>
2020-12-17 22:52:40 +0000

    Day 17 - Part 2 Completed (somehow)

--- day17/day17.py
+++ day17/day17.py
@@ -23,20 +23,6 @@ for lineIndex, line in enumerate(f.split("\n")):
         row[index] = cube
     startingLayer[lineIndex] = row
 
-def getNewCube(minZ, maxZ, minY, maxY, minX, maxX):
-    # print("CREATING NEW CUBE")
-    # print(minZ, maxZ, minY, maxY, minX, maxX)
-    newCube = dict()
-    for z in range(minZ, maxZ + 1):
-        layer = dict()
-        for y in range(minY, maxY + 1):
-            row = dict()
-            for x in range(minX, maxX + 1):
-                row[x] = "."
-            layer[y] = row
-        newCube[z] = layer
-    return newCube
-
 def countActiveCubes(cube):
     total = 0
     for z in range(min(cube), max(cube) + 1):
@@ -44,6 +30,16 @@ def countActiveCubes(cube):
             for x in range(min(cube[0][0]), max(cube[0][0]) + 1):
                 if cube[z][y][x] == "#":
                     total += 1
+    return total
+
+def countActiveCubesHyper(cube):
+    total = 0
+    for w in range(min(cube), max(cube) + 1):
+        for z in range(min(cube[0]), max(cube[0]) + 1):
+            for y in range(min(cube[0][0]), max(cube[0][0]) + 1):
+                for x in range(min(cube[0][0][0]), max(cube[0][0][0]) + 1):
+                    if cube[w][z][y][x] == "#":
+                        total += 1
     return total
 
 def printCube(cube):
@@ -62,10 +58,15 @@ def getCell(cube, z, y, x):
                 return cube.get(z).get(y).get(x)
     return "."
 
-# This appears to be working as expected
+def getCellHyper(cube, w, z, y, x):
+    if cube.get(w, None) != None:
+        if cube.get(w).get(z, None) != None:
+            if cube.get(w).get(z).get(y, None) != None:
+                if cube.get(w).get(z).get(y).get(x, None) != None:
+                    return cube.get(w).get(z).get(y).get(x)
+    return "."
+
 def countActiveNeighbours(pocketCube, zIndex, yIndex, xIndex):
-    # print(zIndex, yIndex, xIndex)
-    # print(getCell(pocketCube, zIndex, yIndex, xIndex))
     totalCounted = 0
     total = 0
     for z in range(zIndex - 1, zIndex + 2):
@@ -76,6 +77,20 @@ def countActiveNeighbours(pocketCube, zIndex, yIndex, xIndex):
                     if getCell(pocketCube, z, y, x) == "#":
                         total += 1
     assert(totalCounted == 26)
+    return total
+
+def countActiveNeighboursHyper(pocketCube, wIndex, zIndex, yIndex, xIndex):
+    totalCounted = 0
+    total = 0
+    for w in range(wIndex - 1, wIndex + 2):
+        for z in range(zIndex - 1, zIndex + 2):
+            for y in range(yIndex - 1, yIndex + 2):
+                for x in range(xIndex - 1, xIndex + 2):
+                    if w != wIndex or z != zIndex or y != yIndex or x != xIndex:
+                        totalCounted += 1
+                        if getCellHyper(pocketCube, w, z, y, x) == "#":
+                            total += 1
+    assert(totalCounted == 80)
     return total
 
 def solvePart1():
@@ -106,7 +121,35 @@ def solvePart1():
     return str(countActiveCubes(pocketCube))
 
 def solvePart2():
-    return "2"
+    hyperCube = dict()
+    hyperCube[0] = dict()
+    hyperCube[0][0] = startingLayer
+
+    cycleCount = 0
+    while cycleCount < 6:
+        thisCycleCube = dict()
+        for w in range(min(hyperCube) - 1, max(hyperCube) + 2):
+            pocketCube = dict()
+            for z in range(min(hyperCube[0]) - 1, max(hyperCube[0]) + 2):
+                layer = dict()
+                for y in range(min(hyperCube[0][0]) - 1, max(hyperCube[0][0]) + 2):
+                    row = dict()
+                    for x in range(min(hyperCube[0][0][0]) - 1, max(hyperCube[0][0][0]) + 2):
+                        currentCell = getCellHyper(hyperCube, w, z, y, x)
+                        activeNeighbours = countActiveNeighboursHyper(hyperCube, w, z, y, x)
+                        if currentCell == ".":
+                            if activeNeighbours == 3:
+                                currentCell = "#"
+                        elif currentCell == "#":
+                            if activeNeighbours != 2 and activeNeighbours != 3:
+                                currentCell = "."
+                        row[x] = currentCell
+                    layer[y] = row
+                pocketCube[z] = layer
+            thisCycleCube[w] = pocketCube
+        hyperCube = thisCycleCube
+        cycleCount += 1
+    return str(countActiveCubesHyper(hyperCube))
 
 print("Part 1: " + solvePart1())
 print("Part 2: " + solvePart2())
