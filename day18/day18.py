899ec963484a420e127407374b5917122675d7e8
Mike Conroy <mike.conroy@appirio.com>
2020-12-21 13:04:37 +0000

    Day 18 - Part 1 Completed.

--- day18/day18.py
+++ day18/day18.py
@@ -34,58 +34,64 @@ class leafNode:
     def __init__(self, value):
         self.value = value
 
+def countTokensInString(string, token):
+    total = 0
+    for char in string:
+        if char == token:
+            total += 1
+    return total
+
 def evaluate(expression):
-    print("EVALUATING:", expression)
+    print(expression)
     sum = 0
-    stack = 0
-    subExpression = False
-    subExpressionStr = ""
     currentOp = "nop"
+    stack = 0
+    subExpression = ""
     for token in expression.split(" "):
-        if subExpression:
-            subExpressionStr += " " + token.replace(")", "")
+        if stack >= 1:
             if token.endswith(")"):
-                if stack == 1:
-                    subExpression = False
-                    if currentOp == "nop":
-                        sum = evaluate(subExpressionStr)
-                    elif currentOp == "*":
-                        sum *= evaluate(subExpressionStr)
-                    elif currentOp == "+":
-                        sum += evaluate(subExpressionStr)
-                    subExpressionStr = ""
+                stack -= countTokensInString(token, ")")
+                if stack < 1:
+                    subExpression += token[:-1] + " "
                 else:
-                    stack -= 1
+                    subExpression += token + " "
+                if stack == 0:
+                    if currentOp == "+":
+                        sum += evaluate(subExpression)
+                    elif currentOp == "*":
+                        sum *= evaluate(subExpression)
+                    elif currentOp == "nop":
+                        sum = evaluate(subExpression)
+                    subExpression = ""
+            elif token.startswith("("):
+                stack += countTokensInString(token, "(")
+                subExpression += token + " "
+            else:
+                subExpression += token + " "
         else:
-            if token.isdigit():
+            if token.startswith("("):
+                stack += countTokensInString(token, "(")
+                subExpression += token[1:] + " "
+            elif token.isdigit():
                 if currentOp == "nop":
                     sum = int(token)
-                elif currentOp == "*":
-                    sum *= int(token)
                 elif currentOp == "+":
                     sum += int(token)
-            elif token in ["+", "*"]:
+                elif currentOp == "*":
+                    sum *= int(token)
+            elif token in ["*", "+"]:
                 currentOp = token
-        if token.startswith("("):
-            stack += 1
-            token = token.replace("(", "")
-            subExpressionStr += token
-            subExpression = True
     return sum
 
 def solvePart1():
     # print("RESULT:", expression("1 + 2 * 3 + 4 * 5 + 6").evaluate())
     # Loop over each expression
-    # sum = 0
-    # for line in lines:
-    #     currentExpression = expression(line)
-    #     sum += currentExpression.evaluate()
-    #     print(line)
-    print(evaluate("1 + 2 * 3 + 4 * 5 + 6"))
-    expression = "1 + (2 * 3) + 4 * (5 + 6)"
-    return str(evaluate(expression))
-
-    return str(0)
+    sum = 0
+    for line in lines:
+        sum += evaluate(line)
+        print(line)
+    
+    return str(sum)
 
 def solvePart2():
     return "2"
